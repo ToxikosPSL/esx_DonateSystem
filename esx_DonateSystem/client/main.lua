ad04dc89b9f2c86ce33b214342cb6a3c4508b39e
ToxikosPSL <130987468+ToxikosPSL@users.noreply.github.com>
2023-04-17 14:34:48 +0300

    Delete esx_DonateSystem directory

--- esx_DonateSystem/client/main.lua
+++
@@ -1,6 +0,0 @@
-local Keys = {
-	["ESC"] = 322, ["BACKSPACE"] = 177, ["E"] = 38, ["ENTER"] = 18,	["LEFT"] = 174, ["RIGHT"] = 175, ["TOP"] = 27, ["DOWN"] = 173
-}
-
-ESX = nil
-TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
